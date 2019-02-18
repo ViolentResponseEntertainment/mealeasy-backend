1ddb93c23438daf4dd6c0aadbb6e53df193851c4
Scott Lackey <thomasscottlackey@gmail.com>
2019-02-18 13:53:02 -0600

    scaffolded loopback 4 app

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {MealeasyBackendApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new MealeasyBackendApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: MealeasyBackendApplication;
+  client: Client;
+}
